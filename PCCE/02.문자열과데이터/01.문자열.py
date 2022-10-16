b80dec81a49e4a69c3395b445eeb527da75a22db
BenKang <rksk1588@gmail.com>
2022-10-17 00:34:04 +0900

    2022.10.17

---
+++ PCCE/02.문자열과데이터/01.문자열.py
@@ -0,0 +1,17 @@
+# 문자열 (string)
+# : 문자형태의 데이터
+# : " 또는 '로 문자열의 시작과 끝을 나타냄
+
+# "this is string"
+# 'this is string'
+
+# "He thinks, 'I am hungry...'"
+# 'She says, "You should go home"'
+
+# escape 문자
+# "He thinks, \"I am hungry...\""
+# 'She says, \'You should go home\''
+# "I want to push ENTER.\n"
+
+print("He thinks, \"I am hungry...\"");
+print("I want to push ENTER.\nasdf");
