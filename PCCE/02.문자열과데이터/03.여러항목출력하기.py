a6d30ae86617ec0e23e09b20feebd486bc6a68c3
BenKang <rksk1588@gmail.com>
2022-10-18 00:41:35 +0900

    2022.10.18

---
+++ PCCE/02.문자열과데이터/03.여러항목출력하기.py
@@ -0,0 +1,15 @@
+# 1. 콤마로 구분해서 출력하기
+print("초코릿", "사탕", "아이스크림");
+
+# 2. 변수에 저장해서 출력하기
+food1 = "초콜릿"
+food2 = "사탕"
+food3 = "아이스크림"
+print(food1,food2,food3,"먹고싶어요");
+
+# 3. 문자열 연산
+string1 = "samsung" + "phone"
+string2 = "My"
+string3 = string2 + string1
+print(string1);
+print(string3);
