8cc665384a373e895956842eb9a3e854f9503608
BenKang <rksk1588@gmail.com>
2022-10-18 01:01:09 +0900

    2022.10.18

---
+++ PCCE/02.문자열과데이터/05.실습문제5.py
@@ -0,0 +1,6 @@
+# 5
+country = "South Korea"
+city = "Seoul"
+region = "Gang-nam"
+print(country, city, region)
+print(country, city, region, sep="\n")  # sep = separator
---
+++ PCCE/02.문자열과데이터/06.실습문제6.py
@@ -0,0 +1,8 @@
+# 6
+singer1 = "Justin Bieber"
+singer2 = "Charlie Puth"
+song1 = "Off My Face"
+song2 = "That's Hilarious"
+print("Best Pop Songs");
+print(singer1 + "-" + song1);
+print(singer2 + "-" + song2);
