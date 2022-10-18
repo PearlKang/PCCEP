ce1a5862d9911e119b3f99c382e18a1a4fb0fa5a
BenKang <rksk1588@gmail.com>
2022-10-19 01:18:57 +0900

    2022.10.19

---
+++ PCCE/03.숫자데이터/07.실습문제12.py
@@ -0,0 +1,8 @@
+# 12
+n=int(input())
+sum=n*(n+1)/2
+print(sum)
+sum=int(n*(n+1)/2)
+print(sum)
+sum=n*(n+1)//2
+print(sum)
---
+++ PCCE/03.숫자데이터/08.실습문제13.py
@@ -0,0 +1,7 @@
+# 13
+n=int(input())
+print(1%n)
+print(2%n)
+print(3%n)
+print(4%n)
+print(5%n)
