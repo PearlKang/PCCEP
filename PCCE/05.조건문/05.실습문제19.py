9a16d25bcf5608ce766aa7f40839f857f085bb2a
BenKang <rksk1588@gmail.com>
2022-10-20 00:46:20 +0900

    2022.10.20

---
+++ PCCE/05.조건문/05.실습문제19.py
@@ -0,0 +1,11 @@
+# 19
+money=int(input("현재 가진 금액 입력(원):"))
+
+if money>=20000:
+    print("오늘 저녁은...치킨!")
+elif money>=10000:
+    print("오늘 저녁은...떡볶이!")
+elif money>=2000:
+    print("오늘 저녁은...편의점 김밥!")
+else:
+    print("굶자")
