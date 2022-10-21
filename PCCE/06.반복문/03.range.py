ffcaeeada8fe6dac4a35c0ebced9d9530ed53d06
BenKang <rksk1588@gmail.com>
2022-10-21 21:34:57 +0900

    2022.10.21

---
+++ PCCE/06.반복문/03.range.py
@@ -0,0 +1,15 @@
+# range(start, end)
+# for i in range(1,6):
+#     print(i, "페이지 크롤링 중...")
+
+# 1부터 n까지 출력
+# n=int(input())
+# for i in range(1,n+1):
+#     print(i)
+
+# range(start, end, step)
+# for i in range(1,11,2):
+#     print(i)
+
+for i in range(10,0,-1):
+    print(i)
