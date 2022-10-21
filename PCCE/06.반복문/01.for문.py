3c430b62580192d057d9c72958fa12330a7969a0
BenKang <rksk1588@gmail.com>
2022-10-21 21:24:20 +0900

    2022.10.21

---
+++ PCCE/06.반복문/01.for문.py
@@ -0,0 +1,12 @@
+# 반복문
+# : 특정 명령을 반복해서 수행하게 만들 때 사용
+
+# for i in range(5):
+#     print("Learn with startcoding!")
+
+# for i in range(5):
+#     print(i)
+
+for i in range(5):
+    print(i+1, "번째 작업 중...")
+    print("명령을 수행")
