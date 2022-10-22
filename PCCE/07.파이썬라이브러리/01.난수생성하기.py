d948c821b70a30703fbe93551fd7a8ac5c919174
BenKang <rksk1588@gmail.com>
2022-10-22 13:35:41 +0900

    2022.10.22

---
+++ PCCE/07.파이썬라이브러리/01.난수생성하기.py
@@ -0,0 +1,5 @@
+import random
+
+# 1 ~ 10 사이 랜덤한 정수 생성
+n=random.randint(1,10)
+print(n)
---
+++ PCCE/07.파이썬라이브러리/02.시간멈추기.py
@@ -0,0 +1,10 @@
+from time import time
+
+
+import time
+
+print("안녕하세요.")
+time.sleep(2)
+print("이 메세지는 2초 뒤에 출력 됩니다.")
+time.sleep(1.5)
+print("이 메세지는 3.5초 뒤에 출력 됩니다.")
