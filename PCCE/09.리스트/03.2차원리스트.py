2c52e13a8a33d4d3a0bb0554e4296e68f1c42842
BenKang <rksk1588@gmail.com>
2022-10-24 19:50:39 +0900

    2022.10.24

---
+++ PCCE/09.리스트/03.2차원리스트.py
@@ -0,0 +1,12 @@
+person_info_list=[
+    ["유재석",51,"남"],
+    ["노홍철",44,"남"],
+    ["하하",44,"남"]
+]
+
+print(person_info_list[0][0])
+print(person_info_list[2][1])
+
+person_info_list.append(["정준하",50,"남"])
+
+print(person_info_list)
