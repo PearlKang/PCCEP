ea6548fe2fe9c17114141643be5e22e46fe7ae12
BenKang <rksk1588@gmail.com>
2022-10-28 12:52:25 +0900

    2022.10.28

---
+++ PCCE/13.PCCE모의고사2/03.문제03.py
@@ -0,0 +1,15 @@
+# 65 D
+# 98 A
+
+score=int(input())
+
+if score>=90:
+    print("A")
+elif score>=80:
+    print("B")
+elif score>=70:
+    print("C")
+elif score>=60:
+    print("D")
+else:
+    print("F")
