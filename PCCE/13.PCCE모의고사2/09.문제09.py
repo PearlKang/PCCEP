31ff4ae740cec83d6f443c59e0be201364b38e98
BenKang <rksk1588@gmail.com>
2022-10-29 17:42:52 +0900

    2022.10.29

--- PCCE/13.PCCE모의고사2/09.문제09.py
+++ PCCE/13.PCCE모의고사2/09.문제09.py
@@ -39,3 +39,95 @@ def solution(serial):
 
 print(solution("01100103"))
 print(solution("02131003"))
+
+def solution2(serial):
+    answer=""
+    gender=serial[:2]
+    part=serial[2:4]
+    team=serial[4:6]
+    vnum=serial[6:]
+
+    # 성별
+    if gender=="01":
+        answer+="male"
+    elif gender=="02":
+        answer+="female"
+    answer+="/"
+
+    # 소속 부서
+    if part=="10":
+        answer+="operation"
+    elif part=="11":
+        answer+="sales"
+    elif part=="12":
+        answer+="develop"
+    elif part=="13":
+        answer+="finance"
+    elif part=="14":
+        answer+="law"
+    elif part=="15":
+        answer+="research"
+    answer+="/"
+
+    # 소속 팀
+    answer+=str(int(team))+"team"
+    answer+="/"
+
+    # 유효성 번호
+    check_sum=0
+    for n in serial[:6]:
+        check_sum+=int(n)
+    
+    if check_sum%13==int(vnum):
+        answer+="valid"
+    else:
+        answer+="invalid"
+
+    return answer
+
+print(solution2("01100103"))
+print(solution2("02131003"))
+
+def solution3(serial):
+    answer=""
+    gender=serial[:2]
+    part=serial[2:4]
+    team=serial[4:6]
+    vnum=serial[6:]
+
+    info={
+        "01":"male",
+        "02":"female",
+        "10":"operation",
+        "11":"sales",
+        "12":"develop",
+        "13":"finance",
+        "14":"law",
+        "15":"research"
+    }
+    # 성별
+    answer+=info[gender]
+    answer+="/"
+
+    # 소속 부서
+    answer+=info[part]
+    answer+="/"
+
+    # 소속 팀
+    answer+=str(int(team))+"team"
+    answer+="/"
+
+    # 유효성 번호
+    check_sum=0
+    for n in serial[:6]:
+        check_sum+=int(n)
+    
+    if check_sum%13==int(vnum):
+        answer+="valid"
+    else:
+        answer+="invalid"
+
+    return answer
+
+print(solution3("01100103"))
+print(solution3("02131003"))
