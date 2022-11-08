86164dcab79843d2263acf9ae952ad54eda8e2be
BenKang <rksk1588@gmail.com>
2022-11-09 00:55:36 +0900

    2022.11.09

--- PCCE/13.PCCE모의고사2/08.문제08.py
+++ PCCE/13.PCCE모의고사2/08.문제08.py
@@ -7,3 +7,23 @@ def solution(num_list):
     return num_list
 
 print(solution([2,4,3,1]))
+
+def solution2(num_list):
+    for n in range(len(num_list)-1):
+        for i in range(len(num_list)-1):
+            if num_list[i]>num_list[i+1]:
+                num_list[i],num_list[i+1]=num_list[i+1],num_list[i]
+
+    return num_list
+
+print(solution2([2,4,3,1]))
+
+def solution3(num_list):
+    for n in range(len(num_list)-1):
+        for i in range(len(num_list)-1-n):
+            if num_list[i]>num_list[i+1]:
+                num_list[i],num_list[i+1]=num_list[i+1],num_list[i]
+
+    return num_list
+
+print(solution3([2,4,3,1]))
