29b4387bf03c78ae3884bacf5f8d08141c8bc315
BenKang <rksk1588@gmail.com>
2022-11-10 00:18:13 +0900

    2022.11.10

--- PCCE/13.PCCE모의고사2/10.문제10+.py
+++ PCCE/13.PCCE모의고사2/10.문제10+.py
@@ -25,3 +25,25 @@ print(solution([[0,1,0],[0,1,0],[0,0,1]])) # 0
 print(solution([[0,0,0],[0,0,1],[1,0,0]])) # 1
 print(solution([[1,1,0],[0,1,1],[1,0,1]])) # 0
 print(solution([[1,0,0],[0,1,0],[1,0,1]])) # 1
+
+def solution2(board):
+    answer=0
+    neighbor=board[0][0]+board[0][1]+board[0][2]+board[1][0]+board[1][2]+board[2][0]+board[2][1]+board[2][2]
+
+    if board[1][1]:
+        if 3<=neighbor<=4:
+            answer=1
+        else:
+            answer=0
+    else:
+        if neighbor==2:
+            answer=1
+        else:
+            answer=0
+
+    return answer
+
+print(solution2([[0,1,0],[0,1,0],[0,0,1]])) # 0
+print(solution2([[0,0,0],[0,0,1],[1,0,0]])) # 1
+print(solution2([[1,1,0],[0,1,1],[1,0,1]])) # 0
+print(solution2([[1,0,0],[0,1,0],[1,0,1]])) # 1
